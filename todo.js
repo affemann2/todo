7b7ea320d4709711591baf04b6a98115ed9a9617
Amit Kumar <email@amitkumar.com>
2017-02-25 17:30:33 -0500

    jquery for some DOM interactions

--- todo.js
+++ todo.js
@@ -1,18 +1,33 @@
 window.onload = function(){
 
-var submit = document.getElementById('submit');
-submit.onclick = function(){
-	var task = document.getElementById('todo').value;
-	console.log(task);
-	var ol = document.getElementById("list")
-	var entry = document.createElement('li');
-	entry.id = 'id'
-	var checkbox = document.createElement('input');
-	checkbox.type = "checkbox";
-	checkbox.id = "checkbox"
-	entry.appendChild(checkbox)
-	entry.appendChild(document.createTextNode(task));
-	ol.appendChild(entry);
+	var submit = document.getElementById('submit');
+	submit.onclick = function(){
+		var task = document.getElementById('todo').value;
+		console.log(task);
+		var ol = document.getElementById("list")
+		var entry = document.createElement('li');
+		
+		var checkbox = document.createElement('input');
+		checkbox.type = "checkbox";
+		checkbox.className = "checkbox";
+		entry.appendChild(checkbox)
+		entry.appendChild(document.createTextNode(task));
+		ol.appendChild(entry);
+
 
 	};
+
+
+	$('#list').on('click', '.checkbox', function(event){
+		var $this = $(this);
+		var $listItem = $this.parent();
+		var text = $listItem.text();
+		// alert(text);
+
+		console.log(text, $this.is(':checked') );
+
+		if ($this.is(':checked')){
+			$listItem.remove();
+		}
+	});
 };
